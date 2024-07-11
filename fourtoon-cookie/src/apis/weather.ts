89e5f1a589e1b3fa8f0e80ebfb41eef464fe4b2a
JuneBridge <junebridge03@g.skku.edu>
2024-07-11 21:40:32 +0900

    feat: getWeather를 구현

--- fourtoon-cookie/src/apis/weather.ts
+++ fourtoon-cookie/src/apis/weather.ts
@@ -1,6 +1,28 @@
 import { Position } from "../types/gps";
 
+const API_URL = "";
+
 export const getWeather = async (date: Date, gpsPos: Position): Promise<string> => {
+
+    const query = {
+        year: date.getFullYear().toString(),
+        month: date.getMonth().toString(),
+        day: date.getDate().toString(),
+        latitude: gpsPos.latitude.toString(),
+        longitude: gpsPos.longitude.toString()
+    }
+
+    const queryString = new URLSearchParams(query).toString();
     
-    return "";
+    const response = await fetch(`${API_URL}/diary?` + queryString, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            ' Authorization': `Bearer ${localStorage.getItem('accessToken')}` // TODO: accessToken 관리에 대한 논의가 필요합니다..!
+        }
+    });
+
+    const data = await response.json();
+
+    return data["weather-id"];
 }
