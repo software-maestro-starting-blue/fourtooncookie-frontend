b801dd29bfb68071aba15f94a6e7bdd0877be48c
minseo Jo <liging12@naver.com>
2024-07-13 19:54:39 +0900

    feat: ImageButton 구현

---
+++ fourtoon-cookie/src/components/common/ImageButton/ImageButton.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { TouchableOpacity, Image, StyleSheet, ViewStyle, ImageStyle } from 'react-native';
+import * as S from './ImageButton.styled';
+
+interface ImageButtonProps {
+    imageUrl: string;
+    onPress: () => void;
+    style?: ViewStyle;
+    imageStyle?: ImageStyle;
+}
+
+const ImageButton = (props: ImageButtonProps) => {
+    const { imageUrl, onPress, style, imageStyle } = props;
+    return (
+        <TouchableOpacity onPress={onPress} style={[S.styles.button, style]}>
+            <Image source={{ uri: imageUrl }} style={[S.styles.image, imageStyle]} />
+        </TouchableOpacity>
+    );
+};
+
+export default ImageButton;
