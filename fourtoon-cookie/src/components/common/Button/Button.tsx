3f139183a684e1985c8df3ded116fe40a2f3b415
minseo Jo <liging12@naver.com>
2024-07-13 19:50:25 +0900

    feat: common - Button 구현

---
+++ fourtoon-cookie/src/components/common/Button/Button.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { TouchableOpacity, Text, StyleSheet, GestureResponderEvent, ViewStyle, TextStyle } from 'react-native';
+import * as S from './Button.styled';
+
+export interface ButtonProps {
+    title: string;
+    onPress: (event: GestureResponderEvent) => void;
+    style?: ViewStyle;
+    textStyle?: TextStyle;
+}
+
+const Button = (props: ButtonProps) => {
+    const { title, onPress, style, textStyle} = props;
+    return (
+        <TouchableOpacity style={[S.styles.button, style]} onPress={onPress}>
+            <Text style={[S.styles.buttonText, textStyle]}>{title}</Text>
+        </TouchableOpacity>
+    );
+}
+
+export default Button;
