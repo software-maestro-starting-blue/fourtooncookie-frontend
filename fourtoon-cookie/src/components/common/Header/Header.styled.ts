ac54bd9e26914ce2d0503c526f41f6cdcb850175
minseo Jo <liging12@naver.com>
2024-07-13 19:52:14 +0900

    feat: Header 구현

---
+++ fourtoon-cookie/src/components/common/Header/Header.styled.ts
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    header: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginBottom: 10,
+    },
+    headerIcon: {
+        width: 30,
+        height: 30,
+        marginRight: 10,
+    },
+    headerText: {
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+});
