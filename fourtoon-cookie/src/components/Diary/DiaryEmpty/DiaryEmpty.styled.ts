dc499f97a20eaa2b618c56b1594e8f8197200709
minseo Jo <liging12@naver.com>
2024-07-18 19:33:01 +0900

    chore: 필요없는 주석 제거

--- fourtoon-cookie/src/components/Diary/DiaryEmpty/DiaryEmpty.styled.ts
+++ fourtoon-cookie/src/components/Diary/DiaryEmpty/DiaryEmpty.styled.ts
@@ -1,4 +1,3 @@
-// DiaryEmpty.styled.ts
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
--- fourtoon-cookie/src/components/Diary/DiaryEmpty/DiaryEmpty.tsx
+++ fourtoon-cookie/src/components/Diary/DiaryEmpty/DiaryEmpty.tsx
@@ -4,16 +4,16 @@ import { View, Text, TouchableOpacity, Image } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import DiaryWritePage from '../../../pages/DiaryWritePage/DiaryWritePage';
 import AddIcon from '../../../../assets/icon/add.png';
-import { styles } from './DiaryEmpty.styled';
+import * as S from './DiaryEmpty.styled';
 
 const DiaryEmpty = () => {
     const navigation = useNavigation();
 
     return (
-        <View style={styles.container}>
-            <TouchableOpacity style={styles.button} onPress={() => navigation.navigate(DiaryWritePage as never)}>
-                <Image source={AddIcon} style={styles.image} />
-                <Text style={styles.text}>Write a diary</Text>
+        <View style={S.styles.container}>
+            <TouchableOpacity style={S.styles.button} onPress={() => navigation.navigate(DiaryWritePage as never)}>
+                <Image source={AddIcon} style={S.styles.image} />
+                <Text style={S.styles.text}>Write a diary</Text>
             </TouchableOpacity>
         </View>
     );
