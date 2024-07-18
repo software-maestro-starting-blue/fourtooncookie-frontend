1d0854effce0d08640452f079b9fdcbd95c9c31c
minseo Jo <liging12@naver.com>
2024-07-18 19:34:05 +0900

    feat: 다이어리 가져오기 요청시 멤버 아이디 헤더에 넣기
    추후 변경 될 예정

--- fourtoon-cookie/src/pages/DiaryTimelinePage/DiaryTimelinePage.tsx
+++ fourtoon-cookie/src/pages/DiaryTimelinePage/DiaryTimelinePage.tsx
@@ -30,7 +30,7 @@ const DiaryTimelinePage = () => {
         const loadDiaries = async () => {
             if (!isLoadingMore && hasMore) {
                 setIsLoadingMore(true);
-                const result = await getDiaries(page);
+                const result = await getDiaries(page, 1); // TODO 1대신 멤버 ID 
                 if (result === null) {
                     setHasMore(false);
                 } else {
