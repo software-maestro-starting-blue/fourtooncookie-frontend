278c0ee7154d3173586dcaa2959e92ecdc18a52a
SproutMj <mjk54565@gmail.com>
2024-07-19 18:06:11 +0900

    merge: conflict 해결

--- fourtoon-cookie/env.d.ts
+++ fourtoon-cookie/env.d.ts
@@ -1,3 +1,6 @@
 declare module '@env' {
     export const API_URL: string;
+    export const SUPABASE_API_URL: string;
+    export const SUPABASE_API_KEY: string;
+    export const CLIENT_ID: string;
 }
