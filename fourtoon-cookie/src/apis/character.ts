577849d95f090df612e7e341b13d6694873ae466
minseo Jo <liging12@naver.com>
2024-07-26 14:58:26 +0900

    feat: getCharacters API 구현

---
+++ fourtoon-cookie/src/apis/character.ts
@@ -0,0 +1,37 @@
+import { API_URL } from "@env";
+import type { Character } from "../types/character";
+import type { CharacterSavedResponse } from "../types/dto/character";
+
+export const getCharacters = async (): Promise<Character[]> => {
+
+    try {
+        const response = await fetch(`${API_URL}/character`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                // ' Authorization': `Bearer ${localStorage.getItem('accessToken')}` // TODO: accessToken 관리에 대한 논의가 필요
+            }
+        });
+        
+        if (response.status === 200) {
+            const data = await response.json();
+            const characterResponses: CharacterSavedResponse[] = data.characterResponses;
+            return characterResponses.map(character => ({
+                id: character.id,
+                characterVisionType: character.characterVisionType,
+                artworkTitle: character.artworkTitle,
+                artworkThumnailUrl: character.artworkThumnailUrl,
+                name: character.name,
+                selectionThumbnailUrl: character.selectionThumbnailUrl
+            }));
+        } else if (response.status === 204) {
+            return [];
+        } else {
+            throw new Error("[GET] getCharacters error");
+        }
+    } catch (error) {
+        console.error("getCharacters : ", error);
+    }
+
+    throw new Error("getCharacters error");
+}
