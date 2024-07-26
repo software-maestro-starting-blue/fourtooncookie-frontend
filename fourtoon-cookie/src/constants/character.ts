a3194cc4fab64cd0b7874fca34a70df80c617e60
minseo Jo <liging12@naver.com>
2024-07-26 15:00:05 +0900

    feat: Character 에서 사용하는 상수 추가

---
+++ fourtoon-cookie/src/constants/character.ts
@@ -0,0 +1,5 @@
+export const FREE_CHARACTER_VISION_TYPES = ['DALL_E_3', 'MIDJOURNEY'];
+export const PAID_CHARACTER_VISION_TYPES = ['STABLE_DIFFUSION'];
+export const FREE = 'free';
+export const PAID = 'paid';
+export type CharacterCategory = typeof FREE | typeof PAID;
