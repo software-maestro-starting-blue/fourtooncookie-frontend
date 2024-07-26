1ebf90a901db519298238bed4dbf0fa5057aa194
minseo Jo <liging12@naver.com>
2024-07-26 14:58:00 +0900

    feat: Character, CharacterSavedResponse 인터페이스 구현

---
+++ fourtoon-cookie/src/types/character.ts
@@ -0,0 +1,8 @@
+export interface Character {
+    id: number,
+    characterVisionType: string,
+    artworkTitle: string,
+    artworkThumnailUrl: string,
+    name: string,
+    selectionThumbnailUrl: string 
+}
---
+++ fourtoon-cookie/src/types/dto/character.ts
@@ -0,0 +1,8 @@
+export interface CharacterSavedResponse {
+    id: number,
+    characterVisionType: string,
+    artworkTitle: string,
+    artworkThumnailUrl: string,
+    name: string,
+    selectionThumbnailUrl: string 
+}
