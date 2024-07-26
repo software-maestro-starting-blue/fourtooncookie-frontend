0981bdc5e0f4d09e661bb4ccdd25748c20f438db
minseo Jo <liging12@naver.com>
2024-07-26 14:59:26 +0900

    feat: 헤더에 들어가는 탭 구현
    무료, 유료

---
+++ fourtoon-cookie/src/pages/CharacterSelectPage/Header/Tab/Tab.styled.ts
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    tab: {
+        flex: 1,
+        alignItems: 'center',
+        paddingVertical: 10,
+    },
+    tabText: {
+        fontSize: 16,
+    },
+    activeTabText: {
+        fontWeight: 'bold',
+        color: 'gray',
+    },
+    
+});
---
+++ fourtoon-cookie/src/pages/CharacterSelectPage/Header/Tab/Tab.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Text, TouchableOpacity } from 'react-native';
+import * as S from './Tab.styled';
+
+interface TabProps {
+    isActive: boolean;
+    label: string;
+    onPress: () => void;
+}
+
+const Tab = (props: TabProps) => {
+    const { isActive, label, onPress } = props;
+
+    return (
+        <TouchableOpacity onPress={onPress} style={S.styles.tab}>
+            <Text style={[S.styles.tabText, isActive && S.styles.activeTabText]}>{label}</Text>
+        </TouchableOpacity>
+    );
+};
+
+export default Tab;
