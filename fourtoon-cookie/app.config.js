2fc669890cb1a99af84c9749358f0333e9c22f8b
SproutMj <mjk54565@gmail.com>
2024-07-27 17:23:24 +0900

    build: eas.json에 민갑정보를 추가하고 제거, 설정을 app.config에 추가

---
+++ fourtoon-cookie/app.config.js
@@ -0,0 +1,49 @@
+export default {
+    expo: {
+        "name": "fourtoon-cookie",
+        "slug": "fourtoon-cookie",
+        "version": "1.0.0",
+        "orientation": "portrait",
+        "icon": "./assets/icon.png",
+        "userInterfaceStyle": "automatic",
+        "splash": {
+            "image": "./assets/splash.png",
+            "resizeMode": "contain",
+            "backgroundColor": "#FFFFFF"
+        },
+        android: {
+            "adaptiveIcon": {
+                "foregroundImage": "./assets/adaptive-icon.png",
+                "backgroundColor": "#ffffff"
+            },
+            package: process.env.ANDROID_PACKAGE
+        },
+        "ios": {
+            "supportsTablet": true,
+            "bundleIdentifier": process.env.BUNDLE_IDENTIFIER,
+            "infoPlist": {
+                "CFBundleURLTypes": [
+                    {
+                        "CFBundleURLSchemes": [
+                            process.env.CF_BUNDLE_URL_SCHEMES
+                        ]
+                    }
+                ]
+            }
+        },
+        "web": {
+            "favicon": "./assets/favicon.png"
+        },
+        "extra": {
+            "eas": {
+                "projectId": process.env.EAS_PROJECT_ID
+            }
+        },
+        "runtimeVersion": {
+            "policy": "appVersion"
+        },
+        "updates": {
+            "url": process.env.EXPO_UPDATES_URL
+        }
+    },
+};
