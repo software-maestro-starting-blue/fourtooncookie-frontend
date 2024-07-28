70f2660aeeb33efcf2e4fd2653fca72ae7b44db0
SproutMj <mjk54565@gmail.com>
2024-07-28 22:23:51 +0900

    chore: config 설정 변경

--- fourtoon-cookie/app.config.js
+++ fourtoon-cookie/app.config.js
@@ -16,11 +16,13 @@ export default {
                 "foregroundImage": "./assets/adaptive-icon.png",
                 "backgroundColor": "#ffffff"
             },
-            package: process.env.ANDROID_PACKAGE
+            package: process.env.ANDROID_PACKAGE,
+            googleServicesFile: process.env.ANDROID_GOOGLE_SERVICES_FILE
         },
-        "ios": {
+        ios: {
             "supportsTablet": true,
             "bundleIdentifier": process.env.BUNDLE_IDENTIFIER,
+            googleServicesFile: process.env.IOS_GOOGLE_SERVICES_FILE,
             "infoPlist": {
                 "CFBundleURLTypes": [
                     {
