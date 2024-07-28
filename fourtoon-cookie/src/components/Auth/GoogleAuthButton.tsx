c9dd4a2de378e1e4480b41b681473833aa04a8ba
SproutMj <mjk54565@gmail.com>
2024-07-28 22:28:26 +0900

    refactor: 함수 분리

--- fourtoon-cookie/src/components/Auth/GoogleAuthButton.tsx
+++ fourtoon-cookie/src/components/Auth/GoogleAuthButton.tsx
@@ -12,40 +12,44 @@ const GoogleAuthButton = () => {
         webClientId: CLIENT_ID,
     })
 
+    const handleOnPress = () => {
+        return async () => {
+            try {
+                await GoogleSignin.hasPlayServices()
+                const userInfo = await GoogleSignin.signIn()
+
+                if (userInfo.idToken) {
+                    const {data, error} = await supabase.auth.signInWithIdToken({
+                        provider: 'google',
+                        token: userInfo.idToken,
+                    })
+                    //TODO: 토큰 사용
+                } else {
+                    throw new Error('no ID token present!')
+                }
+            } catch (error: any) {
+                if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+                    //TODO: 예외처리
+                    // user cancelled the login flow
+                } else if (error.code === statusCodes.IN_PROGRESS) {
+                    //TODO: 예외처리
+                    // operation (e.g. sign in) is in progress already
+                } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+                    //TODO: 예외처리
+                    // play services not available or outdated
+                } else {
+                    //TODO: 예외처리
+                    // some other error happened
+                }
+            }
+        };
+    }
+
     return (
         <GoogleSigninButton
             size={GoogleSigninButton.Size.Wide}
             color={GoogleSigninButton.Color.Dark}
-            onPress={async () => {
-                try {
-                    await GoogleSignin.hasPlayServices()
-                    const userInfo = await GoogleSignin.signIn()
-
-                    if (userInfo.idToken) {
-                        const { data, error } = await supabase.auth.signInWithIdToken({
-                            provider: 'google',
-                            token: userInfo.idToken,
-                        })
-                        //TODO: 토큰 사용
-                    } else {
-                        throw new Error('no ID token present!')
-                    }
-                } catch (error: any) {
-                    if (error.code === statusCodes.SIGN_IN_CANCELLED) {
-                        //TODO: 예외처리
-                        // user cancelled the login flow
-                    } else if (error.code === statusCodes.IN_PROGRESS) {
-                        //TODO: 예외처리
-                        // operation (e.g. sign in) is in progress already
-                    } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
-                        //TODO: 예외처리
-                        // play services not available or outdated
-                    } else {
-                        //TODO: 예외처리
-                        // some other error happened
-                    }
-                }
-            }}
+            onPress={handleOnPress()}
         />
     )
 }
