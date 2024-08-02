2746a96367dabdaa82b5fdaa985509322c9e2781
JuneBridge <junebridge03@g.skku.edu>
2024-08-02 17:11:11 +0900

    feat: SignUpPage 기본 로직 틀 구성

--- fourtoon-cookie/src/pages/SingUpPage/SignUpPage.tsx
+++ fourtoon-cookie/src/pages/SingUpPage/SignUpPage.tsx
@@ -1,6 +1,32 @@
+import { useEffect, useRef } from "react";
 import * as S from "./SignUpPage.styled";
+import { Gender } from "../../types/gender";
+
+enum SignUpProgres {
+    NAME = 0,
+    BIRTH = 1,
+    GENDER = 2
+}
 
 const SignUpPage = () => {
+    const nameRef = useRef<string>('');
+    const birthRef = useRef<Date>(new Date());
+    const genderRef = useRef<Gender>(Gender.MALE);
+
+    const signUpProgressRef = useRef<SignUpProgres>(SignUpProgres.NAME);
+
+    useEffect(() => {
+        // TODO progress bar 애니메이션 구축
+    }, [signUpProgressRef]);
+
+    const handleNextButtonClick = () => {
+        if (signUpProgressRef.current < SignUpProgres.GENDER){
+            signUpProgressRef.current++;
+        } else {
+            // TODO 회원가입 요청
+            // TODO 페이지 이동
+        }
+    }
 
     return ();
 
