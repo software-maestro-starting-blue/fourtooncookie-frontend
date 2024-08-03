3b1c785474183fd7bd5f3f7c053f3dfa344254aa
JuneBridge <junebridge03@g.skku.edu>
2024-08-03 21:04:45 +0900

    feat: 캐릭터에 대한 정보를 global로 받아오는 로직을 추가하였습니다.

--- fourtoon-cookie/src/pages/SettingPage/CharacterInfoLayout/CharacterInfoLayout.tsx
+++ fourtoon-cookie/src/pages/SettingPage/CharacterInfoLayout/CharacterInfoLayout.tsx
@@ -2,17 +2,22 @@ import { Image, Text, TouchableOpacity, View } from 'react-native';
 import * as S from "./CharacterInfoLayout.styled";
 import { NavigationProp, useNavigation } from '@react-navigation/native';
 import { RootStackParamList } from '../../../constants/routing';
+import { Character } from '../../../types/character';
+import { useContext } from 'react';
+import GlobalJwtTokenStateContext from '../../../components/global/GlobalJwtToken/GlobalJwtTokenStateContext';
+import GlobalSelectionCharacterStateContext from '../../../components/global/GlobalSelectionCharacter/GlobalSelectionCharacterStateContext';
 
 
 
 const CharacterInfoLayout = () => {
     const navigation = useNavigation<NavigationProp<RootStackParamList>>();
+    const character: Character | null = useContext(GlobalSelectionCharacterStateContext).selectedCharacter;
 
     const onCharacterChangeButtonPress = () => {
         navigation.navigate('CharacterSelectPage');
     }
 
-    return (
+    return ( // TODO: 캐릭터 이미지 추가
         <View style={S.styles.character}>
             <Image
                 style={S.styles.characterCircle}
