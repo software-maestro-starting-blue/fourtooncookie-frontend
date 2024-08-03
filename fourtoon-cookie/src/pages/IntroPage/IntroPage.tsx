5e585d815885988836d11245fb37ae36f9de5a89
JuneBridge <junebridge03@g.skku.edu>
2024-08-03 20:09:36 +0900

    feat: token 관리 로직 구현

--- fourtoon-cookie/src/pages/IntroPage/IntroPage.tsx
+++ fourtoon-cookie/src/pages/IntroPage/IntroPage.tsx
@@ -2,11 +2,55 @@ import { View, TouchableOpacity, Image, Text } from "react-native";
 import * as S from './IntroPage.styled';
 import GoogleSignInAndSignUpButton from "../../components/auth/GoogleSignInAndSignUpButton/GoogleSignInAndSignUpButton";
 import { JWTToken } from "../../types/jwt";
+import { NavigationProp, useNavigation } from "@react-navigation/native";
+import { RootStackParamList } from "../../constants/routing";
+import { useContext, useEffect } from "react";
+import GlobalJwtTokenStateContext from "../../components/global/GlobalJwtToken/GlobalJwtTokenStateContext";
+import { supabaseRefreshToken } from "../../apis/supabase";
+import { Member } from "../../types/member";
+import { getMember } from "../../apis/member";
 
 const IntroPage = () => {
+    const navigation = useNavigation<NavigationProp<RootStackParamList>>();
+    const { jwtToken, setJwtToken } = useContext(GlobalJwtTokenStateContext);
 
-    const handleSignInButtonPress = (token: JWTToken) => {
+    useEffect(() => {
+        if (jwtToken) {
+            if (!jwtToken.expires_at || jwtToken.expires_at < Date.now()) {
+                const refreshToken = async () => {
+                    try {
+                        const newToken = await supabaseRefreshToken(jwtToken.refreshToken);
+                        setJwtToken(newToken);
+                    } catch (e) {
+                        setJwtToken(null);
+                    }
+                }
 
+                refreshToken();
+                return;
+            }
+
+            const checkIsFirstTime = async () => {
+                const member: Member = await getMember({jwtToken, setJwtToken});
+                if (member.name == null) {
+                    navigation.navigate('SignUpPage');
+                    return;
+                } else {
+                    navigation.navigate('DiaryTimelinePage');
+                    return;
+                }
+            }
+
+            checkIsFirstTime();
+        }
+    }, [jwtToken, setJwtToken, navigation]);
+
+    if (jwtToken) {
+        return null;
+    }
+
+    const handleSignUpAndSignInSuccess = (token: JWTToken) => {
+        setJwtToken(token);
     }
 
     return (
@@ -17,7 +61,7 @@ const IntroPage = () => {
             <Text style={S.styles.description}>나의 하루를 그림일기로 표현해보세요</Text>
             <View style={S.styles.buttons}>
                 <View style={S.styles.buttonGoogle}>
-                    <GoogleSignInAndSignUpButton onSuccess={handleSignInButtonPress} />
+                    <GoogleSignInAndSignUpButton onSuccess={handleSignUpAndSignInSuccess} />
                 </View>
                 <View style={S.styles.buttonApple}>
                     
