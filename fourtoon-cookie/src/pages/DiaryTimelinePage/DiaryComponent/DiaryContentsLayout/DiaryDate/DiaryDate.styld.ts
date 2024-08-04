edcc682cde72fce2303c169c88477d3e982fcb41
JuneBridge <junebridge03@g.skku.edu>
2024-08-04 17:06:57 +0900

    chore: 필요없는 import 삭제

--- fourtoon-cookie/src/pages/DiaryTimelinePage/DiaryComponent/DiaryContentsLayout/DiaryDate/DiaryDate.styld.ts
+++ fourtoon-cookie/src/pages/DiaryTimelinePage/DiaryComponent/DiaryContentsLayout/DiaryDate/DiaryDate.styld.ts
@@ -1,6 +1,5 @@
 import { Header } from '@react-navigation/stack';
 import { StyleSheet } from 'react-native';
-import DiaryActions from '../DiaryActionLayout/DiaryActions/DiaryActions';
 
 export const styles = StyleSheet.create({
     date: {
