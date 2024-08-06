df060dd30d8fa5dbda5b84ad5e9bfb547e7be018
JuneBridge <junebridge03@g.skku.edu>
2024-08-06 14:16:07 +0900

    chore: CharacterSelectPage에 대한 일부 네이밍 실수 수정 및 자잘한 수정 진행

--- fourtoon-cookie/src/pages/CharacterSelectPage/CharacterSelectPage.tsx
+++ fourtoon-cookie/src/pages/CharacterSelectPage/CharacterSelectPage.tsx
@@ -1,12 +1,10 @@
 import React, { useEffect, useState, useMemo, useContext } from 'react';
 import { Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import {NavigationProp, useNavigation} from "@react-navigation/native";
 import { getCharacters } from '../../apis/character';
-import BackButton from '../../components/common/BackButton/BackButton';
 import GlobalSelectionCharacterStateContext from '../../components/global/GlobalSelectionCharacter/GlobalSelectionCharacterStateContext';
 import ArtworkList from './ArtworkList/ArtworkList';
-import Tabs from './Tabs/Tabs';
+import TabsLayout from './TabsLayout/TabsLayout';
 import { Character, CharacterPaymentType } from '../../types/character';
 import * as S from './CharacterSelectPage.styled';
 import { RootStackParamList } from '../../constants/routing';
@@ -22,52 +20,55 @@ const CharacterSelectPage = () => {
 
     const { selectedCharacter, setSelectedCharacter } = useContext(GlobalSelectionCharacterStateContext);
     const jwtContext = useContext(GlobalJwtTokenStateContext);
-	  const { errorInfo, setErrorInfo } = useContext(GlobalErrorInfoStateContext);
+	const { errorInfo, setErrorInfo } = useContext(GlobalErrorInfoStateContext);
 
     useEffect(() => {
-      const fetchCharacters = async () => {
-        try {
-          const characters = await getCharacters(jwtContext);
-          setCharacters(characters);
-        } catch (e) {
-          if (e instanceof Error) {
-            setErrorInfo({
-              type: GlobalErrorInfoType.MODAL,
-              error: e
-            });
-          }
-        } finally {
-          setLoading(false);
-        }
-      };
+		const fetchCharacters = async () => {
+			try {
+                const characters = await getCharacters(jwtContext);
+                setCharacters(characters);
+			} catch (e) {
+                if (e instanceof Error) {
+                    setErrorInfo({
+                        type: GlobalErrorInfoType.MODAL,
+                        error: e
+                    });
+                }
+			} finally {
+			    setLoading(false);
+			}
+		};
 
-      fetchCharacters();
+		fetchCharacters();
     }, []);
 
     const groupByArtworkTitle = (characters: Character[]) => {
-      return characters.reduce((acc, character) => {
-        if (!acc[character.artworkTitle]) {
-          acc[character.artworkTitle] = [];
-        }
-        acc[character.artworkTitle].push(character);
-        return acc;
-      }, {} as Record<string, Character[]>);
+		return characters.reduce((acc, character) => {
+			if (!acc[character.artworkTitle]) {
+				acc[character.artworkTitle] = [];
+			}
+			acc[character.artworkTitle].push(character);
+			return acc;
+		}, {} as Record<string, Character[]>);
     };
 
     const getCharactersByType = (type: CharacterPaymentType, characters: Character[]) => 
-      characters.filter(character => 
-        type === CharacterPaymentType.FREE 
-          ? character.paymentType === CharacterPaymentType.FREE
-          : character.paymentType === CharacterPaymentType.PAID
-
-      );
+      	characters.filter(character => 
+			type === CharacterPaymentType.FREE 
+			? character.paymentType === CharacterPaymentType.FREE
+			: character.paymentType === CharacterPaymentType.PAID
+		);
       
     const groupedFreeCharacters = useMemo(() => groupByArtworkTitle(getCharactersByType(CharacterPaymentType.FREE, characters)), [characters]);
     const groupedPaidCharacters = useMemo(() => groupByArtworkTitle(getCharactersByType(CharacterPaymentType.PAID, characters)), [characters]);
 
     const handleCharacterPress = (character: Character) => {
-      setSelectedCharacter(character);
+        setSelectedCharacter(character);
     };
+
+    const handleSelectedPaymentTypeChange = (characterPaymentType: CharacterPaymentType) => {
+        setSelectedPaymentType(characterPaymentType);
+    }
 
     if (loading) {
         return (
@@ -80,28 +81,28 @@ const CharacterSelectPage = () => {
     return (
         <SafeAreaView style={S.styles.container}>
             <Header />
-
-            <Tabs
+            <TabsLayout
                 selectedPaymentType={selectedPaymentType}
-                setSelectedPaymentType={setSelectedPaymentType}
+                onSelectedPaymentTypeChange={handleSelectedPaymentTypeChange}
             />
-            
             <View style={S.styles.separator} />
-
             <View>
-                {selectedPaymentType === CharacterPaymentType.FREE ? (
-                    <ArtworkList
+				{
+					(selectedPaymentType === CharacterPaymentType.FREE) &&
+					<ArtworkList
                         groupedCharacters={groupedFreeCharacters}
                         selectedCharacter={selectedCharacter}
                         handleCharacterPress={handleCharacterPress}
                     />
-                ) : (
-                    <ArtworkList
+				}
+				{
+					(selectedPaymentType === CharacterPaymentType.PAID) &&
+					<ArtworkList
                         groupedCharacters={groupedPaidCharacters}
                         selectedCharacter={selectedCharacter}
                         handleCharacterPress={handleCharacterPress}
                     />
-                )}
+				}
             </View>
         </SafeAreaView>
     );
--- fourtoon-cookie/src/pages/CharacterSelectPage/TabsLayout/TabsLayout.tsx
+++ fourtoon-cookie/src/pages/CharacterSelectPage/TabsLayout/TabsLayout.tsx
@@ -1,32 +1,32 @@
 import React from 'react';
 import { View } from 'react-native';
 import Tab from './Tab/Tab';
-import * as S from './Tabs.styled';
+import * as S from './TabsLayout.styled';
 import { FREE_KOR, PAID_KOR } from '../../../constants/character';
 import { CharacterPaymentType } from '../../../types/character';
 
-export interface HeaderProps {
+export interface TabsLayoutProps {
     selectedPaymentType: CharacterPaymentType;
-    setSelectedPaymentType: (category: CharacterPaymentType) => void;
+    onSelectedPaymentTypeChange: (characterPaymentType: CharacterPaymentType) => void;
 }
 
-const Header = (props: HeaderProps) => {
-    const { selectedPaymentType, setSelectedPaymentType, ...rest } = props;
+const TabsLayout = (props: TabsLayoutProps) => {
+    const { selectedPaymentType, onSelectedPaymentTypeChange, ...rest } = props;
     
     return (
         <View style={S.styles.headersContainer}>
             <Tab 
                 isActive={selectedPaymentType === CharacterPaymentType.FREE} 
                 label={FREE_KOR}
-                onPress={() => setSelectedPaymentType(CharacterPaymentType.FREE)} 
+                onPress={() => onSelectedPaymentTypeChange(CharacterPaymentType.FREE)} 
             />
             <Tab 
                 isActive={selectedPaymentType === CharacterPaymentType.PAID} 
                 label={PAID_KOR}
-                onPress={() => setSelectedPaymentType(CharacterPaymentType.PAID)} 
+                onPress={() => onSelectedPaymentTypeChange(CharacterPaymentType.PAID)} 
             />
         </View>
     );
 };
 
-export default Header;
+export default TabsLayout;
