e0cb4f0551c9bdf0b387501cb88ec36652750132
JuneBridge <junebridge03@g.skku.edu>
2024-08-08 17:13:18 +0900

    fix: Resign 핸들링 에러 로직 추가

--- fourtoon-cookie/src/pages/SettingPage/MenuLayout/ResignModal/ResignModal.tsx
+++ fourtoon-cookie/src/pages/SettingPage/MenuLayout/ResignModal/ResignModal.tsx
@@ -2,6 +2,8 @@ import { useContext } from "react";
 import ConfirmationModal from "../../../../components/common/Modal/ConfirmationModal/ConfirmationModal"
 import GlobalJwtTokenStateContext from "../../../../components/global/GlobalJwtToken/GlobalJwtTokenStateContext";
 import { deleteMember } from "../../../../apis/member";
+import GlobalErrorInfoStateContext from "../../../../components/global/GlobalError/GlobalErrorInfoStateContext";
+import { GlobalErrorInfoType } from "../../../../types/error";
 
 export interface ResignModalProps {
     visible: boolean;
@@ -10,11 +12,23 @@ export interface ResignModalProps {
 
 const ResignModal = (props: ResignModalProps) => {
     const { visible, onClose } = props;
+
+    const { errorInfo, setErrorInfo } = useContext(GlobalErrorInfoStateContext);
     const jwtContext = useContext(GlobalJwtTokenStateContext);
 
-    const handleResign = () => {
-        deleteMember(jwtContext);
-        jwtContext.setJwtToken(null);
+    const handleResign = async () => {
+        try {
+            onClose();
+            await deleteMember(jwtContext);
+            jwtContext.setJwtToken(null);
+        } catch (error) {
+            if (error instanceof Error) {
+                setErrorInfo({
+                    type: GlobalErrorInfoType.MODAL,
+                    error: error,
+                });
+            }
+        }
     }
 
     return (
