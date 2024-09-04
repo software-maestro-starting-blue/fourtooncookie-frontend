3ec4dc140fe3b890ff74df859472c9d30b145f58
minseo Jo <liging12@naver.com>
2024-09-04 10:44:29 +0900

    refactor: api.ts 에러 메시지 변경

--- fourtoon-cookie/src/apis/api.ts
+++ fourtoon-cookie/src/apis/api.ts
@@ -10,7 +10,7 @@ export const requestApi = async (url: string, method: string, jwtContext: Global
     const { jwtToken, setJwtToken } = jwtContext;
 
     if (!jwtToken) {
-        throw new JwtError('토큰이 존재하지 않습니다. 다시 로그인해 주세요.');
+        throw new JwtError('사용자 정보가 존재하지 않습니다. 다시 로그인해 주세요.');
     }
 
     const refreshJwtToken = async () => {
@@ -20,7 +20,7 @@ export const requestApi = async (url: string, method: string, jwtContext: Global
             return newToken;
         } catch (error) {
             setJwtToken(null);
-            throw new JwtError('토큰이 존재하지 않습니다. 다시 로그인해 주세요.');
+            throw new JwtError('사용자 정보가 존재하지 않습니다. 다시 로그인해 주세요.');
         }
     };
 
@@ -42,7 +42,7 @@ export const requestApi = async (url: string, method: string, jwtContext: Global
                     const newToken = await refreshJwtToken();
                     return makeRequest(newToken, true);
                 } else {
-                    throw new JwtError(`잘못된 요청입니다. 토큰이 유효하지 않습니다.`);
+                    throw new JwtError(`잘못된 요청입니다. 사용자 정보가 유효하지 않습니다.`);
                 }
             } else {
                 throw new ApiError(`요청이 실패했습니다. 잠시후 다시 시도해 주세요.`);
