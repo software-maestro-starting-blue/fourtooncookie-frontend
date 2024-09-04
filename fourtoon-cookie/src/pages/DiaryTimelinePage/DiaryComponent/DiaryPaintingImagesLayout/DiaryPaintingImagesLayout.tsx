fda67f51e1dceb11304652610e9e27726334e389
minseo Jo <liging12@naver.com>
2024-09-04 19:14:03 +0900

    feat: DiaryPaintingImagesLayout 이미지가 아직 생성되지 않은 경우 그림 그리고 있음을 알려주는 기능 개발
    ### 기능
    Character 가 가지고있는selectionThumbnailUrl 이미지를 보여줌
    
    ### 논의 해야 할 사항
    또한 만약에 캐릭터가 선택되지 않은 경우에는 어떤 이미지를 보여줄 지 선정 해야함. (예: 남녀)
    
    또는 무료캐릭터를 기본캐릭터로 지정해두어도 괜찮아 보인다.

--- fourtoon-cookie/src/pages/DiaryTimelinePage/DiaryComponent/DiaryPaintingImagesLayout/DiaryPaintingImagesLayout.tsx
+++ fourtoon-cookie/src/pages/DiaryTimelinePage/DiaryComponent/DiaryPaintingImagesLayout/DiaryPaintingImagesLayout.tsx
@@ -1,5 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { View, Modal, Image, TouchableOpacity, Text } from "react-native";
+import GlobalSelectionCharacterStateContext from '../../../../components/global/GlobalSelectionCharacter/GlobalSelectionCharacterStateContext';
+
 import * as S from './DiaryPaintingImagesLayout.styled';
 
 export interface DiaryPaintingImagesLayoutProps {
@@ -10,6 +12,7 @@ const DiaryPaintingImagesLayout = (props: DiaryPaintingImagesLayoutProps) => {
     const { imageUrls, ...rest } = props;
     const [isModalVisible, setModalVisible] = useState(false);
     const [selectedImage, setSelectedImage] = useState<string | null>(null);
+    const { selectedCharacter } = useContext(GlobalSelectionCharacterStateContext);
 
     const handleImagePress = (imageUrl: string) => {
         setSelectedImage(imageUrl);
@@ -21,26 +24,48 @@ const DiaryPaintingImagesLayout = (props: DiaryPaintingImagesLayoutProps) => {
         setSelectedImage(null);
     };
 
-    return (
-        <View>
-            <View style={S.styles.grid}>
-                {imageUrls.map((imageUrl, index) => (
-                    <TouchableOpacity style={S.styles.imageContainer} key={index} onPress={() => handleImagePress(imageUrl)}>
-                        <Image
-                            source={{ uri: imageUrl }}
-                            style={S.styles.image}
-                        />
-                    </TouchableOpacity>
-                ))}
+    if (imageUrls.length === 0) {
+        return (
+            <View style={S.styles.emptyContainer}>
+                {selectedCharacter ? (
+                    <Image 
+                        source={{ uri: selectedCharacter.selectionThumbnailUrl }} // 캐릭터의 selectionThumbnailUrl 사용
+                        style={S.styles.characterIcon}
+                    />
+                ) : (
+                    <Image 
+                        source={require('../../../../../assets/logo/logo-3.png')} /** @TODO 캐릭터 기본 이미지 선정 해야함. */
+                        style={S.styles.characterIcon}
+                    />
+                )}
+                <Text style={S.styles.characterText}>
+                    {selectedCharacter?.name || '캐릭터'}가 그림을 그리고 있습니다!
+                </Text>
+                <Text style={S.styles.estimatedTimeText}>약 24시간 소요 예정</Text>
             </View>
-            <ImageModal 
-                isModalVisible={isModalVisible}
-                selectedImage={selectedImage}
-                onCloseModal={handleCloseModal}
-                />
-        </View>
-    );
-};
+        );
+    } else {
+        return (
+            <View>
+                <View style={S.styles.grid}>
+                    {imageUrls.map((imageUrl, index) => (
+                        <TouchableOpacity style={S.styles.imageContainer} key={index} onPress={() => handleImagePress(imageUrl)}>
+                            <Image
+                                source={{ uri: imageUrl }}
+                                style={S.styles.image}
+                            />
+                        </TouchableOpacity>
+                    ))}
+                </View>
+                <ImageModal 
+                    isModalVisible={isModalVisible}
+                    selectedImage={selectedImage}
+                    onCloseModal={handleCloseModal}
+                    />
+            </View>
+        );
+    };
+}
 
 export default DiaryPaintingImagesLayout;
 
