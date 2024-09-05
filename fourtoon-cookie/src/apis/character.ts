d003d4e7af4df8aca77b6edc5f9e8c0ac6184a0e
JuneBridge <junebridge03@g.skku.edu>
2024-09-05 15:18:00 +0900

    refactor: api의 requestApi에 맞추어 모든 api의 function 수정

--- fourtoon-cookie/src/apis/character.ts
+++ fourtoon-cookie/src/apis/character.ts
@@ -1,10 +1,9 @@
 import type { Character } from "../types/character";
 import type { CharacterSavedResponse } from "../types/dto/character";
 import { requestApi } from "./api";
-import { GlobalJwtTokenStateContextProps } from "../components/global/GlobalJwtToken/GlobalJwtTokenStateContext";
 
-export const getCharacters = async (jwtContext: GlobalJwtTokenStateContextProps): Promise<Character[]> => {
-    const response = await requestApi(`/character`, 'GET', jwtContext, undefined);
+export const getCharacters = async (): Promise<Character[]> => {
+    const response = await requestApi(`/character`, 'GET', undefined);
     const characterResponses: CharacterSavedResponse[] = await response.json();
     return [...characterResponses];
 };
