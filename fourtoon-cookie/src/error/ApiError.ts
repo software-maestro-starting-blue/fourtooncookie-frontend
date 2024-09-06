65eb14b135ff244922a90d370cf885faa9d02106
JuneBridge <junebridge03@g.skku.edu>
2024-09-06 10:31:14 +0900

    feat: ApiError에 status property 추가

--- fourtoon-cookie/src/error/ApiError.ts
+++ fourtoon-cookie/src/error/ApiError.ts
@@ -1,7 +1,15 @@
 
 export class ApiError extends Error {
-    constructor(message: string) {
+    
+    private status: number | null;
+
+    constructor(message: string, status: number | null = null) {
         super(message);
         this.name = 'ApiError';
+        this.status = status;
+    }
+
+    getStatus() {
+        return this.status;
     }
 }
