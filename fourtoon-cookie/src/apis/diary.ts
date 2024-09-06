0593d32e5fad1b3c3e993e0f65b738333894b926
JuneBridge <junebridge03@g.skku.edu>
2024-09-06 11:10:09 +0900

    fix: diary api에서 status를 ApiError에 적용하도록 수정

--- fourtoon-cookie/src/apis/diary.ts
+++ fourtoon-cookie/src/apis/diary.ts
@@ -3,9 +3,15 @@ import type { DiaryPatchFavoriteRequest, DiarySaveRequest, DiarySavedResponse, D
 import { requestApi } from "./api";
 import { LocalDate } from "@js-joda/core";
 import { ApiError } from "../error/ApiError";
+import { API_STATUS } from "../constants/api";
 
 export const getDiary = async (diaryId: number): Promise<Diary> => {
     const response = await requestApi(`/diary/${diaryId}`, 'GET');
+
+    if (response.status !== API_STATUS.SUCCESS) {
+        throw new ApiError("일기를 불러오는 중 오류가 발생했습니다.", response.status);
+    }
+
     const diaryResponse: DiarySavedResponse = await response.json();
     return { ...diaryResponse, diaryDate: LocalDate.parse(diaryResponse.diaryDate) };
 }
@@ -13,13 +19,13 @@ export const getDiary = async (diaryId: number): Promise<Diary> => {
 export const getDiaries = async (pageNumber: number): Promise<Diary[]> => {
     const response = await requestApi(`/diary/timeline?pageNumber=${pageNumber}`, 'GET');
 
-    if (response.status === 200) {
+    if (response.status === API_STATUS.SUCCESS) {
         const data: DiarySavedResponse[] = await response.json();
         return data.map(diary => ({...diary, diaryDate: LocalDate.parse(diary.diaryDate)}));
-    } else if (response.status === 204) {
+    } else if (response.status === API_STATUS.NO_CONTENT) {
         return [];
     } else {
-        throw new ApiError("일기 목록을 불러오는 중 오류가 발생했습니다.");
+        throw new ApiError("일기 목록을 불러오는 중 오류가 발생했습니다.", response.status);
     }
 }
 
@@ -33,10 +39,9 @@ export const postDiary = async (characterId: number, date: LocalDate, content: s
     };
 
     const response = await requestApi(`/diary`, 'POST', requestBody);
-    if (response.status === 200) {
-        return;
-    } else {
-        throw new ApiError("일기를 저장하는 중 오류가 발생했습니다. 다시 시도해 주세요.");
+
+    if (response.status != API_STATUS.CREATED) {
+        throw new ApiError("일기를 저장하는 중 오류가 발생했습니다. 다시 시도해 주세요.", response.status);
     }
 }
 
@@ -49,20 +54,16 @@ export const putDiary = async (characterId: number, diaryId: number, content: st
     }; 
     const response = await requestApi(`/diary/${diaryId}`, 'PUT', requestBody);
     
-    if (response.status === 200) {
-        return;
-    } else {
-        throw new ApiError("일기를 수정하는 중 오류가 발생했습니다. 다시 시도해 주세요.");
+    if (response.status != API_STATUS.SUCCESS) {
+        throw new ApiError("일기를 수정하는 중 오류가 발생했습니다. 다시 시도해 주세요.", response.status);
     }
 }
 
 export const deleteDiary = async (diaryId: number): Promise<void> => {
     const response = await requestApi(`/diary/${diaryId}`, 'DELETE');
 
-    if (response.status === 204) {
-        return;
-    } else {
-        throw new ApiError("일기를 삭제하는 중 오류가 발생했습니다. 다시 시도해 주세요.");
+    if (response.status != API_STATUS.NO_CONTENT) {
+        throw new ApiError("일기를 삭제하는 중 오류가 발생했습니다. 다시 시도해 주세요.", response.status);
     }
 };
 
@@ -73,9 +74,7 @@ export const patchDiaryFavorite = async (diaryId: number, isFavorite: boolean): 
 
     const response = await requestApi(`/diary/${diaryId}/favorite`, 'PATCH', requestBody);
 
-    if (response.status === 200) {
-        return;
-    } else {
-        throw new ApiError("일기 즐겨찾기 중 오류가 발생했습니다. 다시 시도해 주세요.");
+    if (response.status != API_STATUS.SUCCESS) {
+        throw new ApiError("일기 즐겨찾기 중 오류가 발생했습니다. 다시 시도해 주세요.", response.status);
     }
 };
