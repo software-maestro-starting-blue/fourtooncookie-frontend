52cceb1ff192b7753d7a90bd3415b72b4f163e2a
JuneBridge <junebridge03@g.skku.edu>
2024-09-06 10:53:30 +0900

    feat: constant를 활용하여서 API method와 status enum을 생성하였습니다.

---
+++ fourtoon-cookie/src/constants/api.ts
@@ -0,0 +1,20 @@
+
+
+export enum API_METHOD_TYPE {
+    GET = 'GET',
+    POST = 'POST',
+    PUT = 'PUT',
+    DELETE = 'DELETE',
+    PATCH = 'PATCH'
+}
+
+export enum API_STATUS {
+    SUCCESS = 200,
+    CREATED = 201,
+    NO_CONTENT = 204,
+    BAD_REQUEST = 400,
+    UNAUTHORIZED = 401,
+    FORBIDDEN = 403,
+    NOT_FOUND = 404,
+    INTERNAL_SERVER_ERROR = 500
+}
