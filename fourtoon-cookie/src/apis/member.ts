e86028af76d4a9e3928af4c57062413feb7621f6
JuneBridge <junebridge03@g.skku.edu>
2024-09-06 11:03:41 +0900

    fix: member api status에 따른 error 핸들링 로직 수정

--- fourtoon-cookie/src/apis/member.ts
+++ fourtoon-cookie/src/apis/member.ts
@@ -4,11 +4,12 @@ import { requestApi } from "./api";
 import type { Member } from "../types/member";
 import { LocalDate } from "@js-joda/core";
 import { ApiError } from "../error/ApiError";
+import { API_STATUS } from "../constants/api";
 
 export const getMember = async (): Promise<Member> => {
     const response = await requestApi(`/member`, 'GET');
 
-    if (response.status != 200) {
+    if (response.status != API_STATUS.SUCCESS) {
         throw new ApiError("회원 정보를 가져오는 중 오류가 발생했습니다."); 
     }
 
@@ -25,14 +26,16 @@ export const postMember = async (name: string, birth: LocalDate, gender: Gender)
     };
 
     const response = await requestApi(`/member`, 'POST', requestBody);
-    if (response.status != 201) {
+
+    if (response.status != API_STATUS.CREATED) {
         throw new ApiError("회원가입 중 오류가 발생했습니다.");
     }
 }
 
 export const deleteMember = async () => {
     const response = await requestApi(`/member`, 'DELETE');
-    if (response.status != 204) {
+
+    if (response.status != API_STATUS.NO_CONTENT) {
         throw new ApiError("회원 정보를 삭제하는 중 오류가 발생했습니다.");
     }
 }
