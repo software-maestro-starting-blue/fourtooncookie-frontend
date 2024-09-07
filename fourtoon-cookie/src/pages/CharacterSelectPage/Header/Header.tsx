d7f618afa9d839ad1f7197e122c977f92856b10f
JuneBridge <junebridge03@g.skku.edu>
2024-09-07 17:58:19 +0900

    feat: CharacterSelectPage에서 캐릭터를 미선택하고 백버튼을 누를 시, 이를 저지하도록 하는 로직 구성
    단, TSK-45에서 에러 처리 방식을 변경함에 따라 에러 호출 로직은 미구현하였습니다.
    TSK-45 반영되면, pull 받고 구현할 예정입니다.

--- fourtoon-cookie/src/pages/CharacterSelectPage/Header/Header.tsx
+++ fourtoon-cookie/src/pages/CharacterSelectPage/Header/Header.tsx
@@ -2,12 +2,20 @@ import { Text, View } from "react-native";
 import BackButton from "../../../components/common/BackButton/BackButton";
 
 import * as S from "./Header.styled";
+import { useContext } from "react";
+import GlobalSelectionCharacterStateContext from "../../../components/global/GlobalSelectionCharacter/GlobalSelectionCharacterStateContext";
 
 const Header = () => {
 
+    const { selectedCharacter } = useContext(GlobalSelectionCharacterStateContext);
+
+    const handleBackButtonPressWhenCharacterNotSelected = () => {
+        //TODO: 백버튼 눌렀을 때 handleError로 처리하기 (TSK-45)
+    }
+
     return (
         <View style={S.styles.header}>
-            <BackButton style={S.styles.backButton} />
+            <BackButton style={S.styles.backButton} onPress={selectedCharacter ? undefined : handleBackButtonPressWhenCharacterNotSelected} />
             <View style={S.styles.textContainer}>
                     <Text style={S.styles.text}>캐릭터 선택</Text>
             </View>
