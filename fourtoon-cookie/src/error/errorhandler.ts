28c92ca66554a5116323b3dea8ad122d91bed7c8
JuneBridge <junebridge03@g.skku.edu>
2024-09-07 16:57:10 +0900

    feat: handleError 함수 구현

---
+++ fourtoon-cookie/src/error/errorhandler.ts
@@ -0,0 +1,32 @@
+import { Alert } from "react-native";
+import { GlobalErrorInfoType } from "../types/error";
+
+
+const handleError = (error: Error, errorType: GlobalErrorInfoType, callback: (error: Error) => void) => {
+    console.error(error);
+
+    switch (errorType) {
+        case GlobalErrorInfoType.MODAL:
+            handleModalError(error, callback);
+            break;
+        default:
+            break;
+    }
+}
+
+const handleModalError = (error: Error, callback: (error: Error) => void) => {
+    Alert.alert(
+        '오류가 발생하였습니다.',
+        error.message,
+        [
+            {
+                text: '확인',
+                onPress: () => {
+                    callback(error);
+                },
+            },
+        ]
+    );
+}
+
+export default handleError;
