90ccc65edd6aa650907530f4756d0db59866d591
SproutMj <mjk54565@gmail.com>
2024-09-07 10:22:13 +0900

    refactor: status가 잘못된 경우에만 에러메시지에 나오도록 변경

--- fourtoon-cookie/src/apis/character.ts
+++ fourtoon-cookie/src/apis/character.ts
@@ -6,13 +6,13 @@ import { requestApi } from "./api";
 export const getCharacters = async (): Promise<Character[]> => {
     const response = await requestApi(`/character`, API_METHOD_TYPE.GET);
     if (response.status !== API_STATUS.SUCCESS) {
-        throw new ApiError('캐릭터를 불러오는 중 오류가 발생했습니다. 잠시 후 다시 시도해 주세요.');
+        throw new ApiError('캐릭터를 불러오는 중 오류가 발생했습니다. 잠시 후 다시 시도해 주세요.', response.status);
     }
 
     const responseData = await response.json();
 
     if (!Array.isArray(responseData.characterSavedResponses)) {
-        throw new ApiError('잘못된 응답 형식입니다. 캐릭터 목록을 불러오지 못했습니다.', response.status);
+        throw new ApiError('잘못된 응답 형식입니다. 캐릭터 목록을 불러오지 못했습니다.');
     }
 
     return responseData.characterSavedResponses as Character[];
