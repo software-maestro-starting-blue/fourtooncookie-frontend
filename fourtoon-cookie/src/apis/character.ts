d8790c3769e298179e88d7aee8142b255fc1c515
KIM MINJUN <mjk54565@gmail.com>
2024-09-07 10:55:20 +0900

    [bug] 캐릭터 배열 변환 오류 해결 (#35)
    * fix: 캐릭터 배열 변환 오류 해결
    
    * refactor: status 제거
    
    * refactor: map함수 없이 반환하도록 변경
    
    * refactor: status가 잘못된 경우에만 에러메시지에 나오도록 변경

--- fourtoon-cookie/src/apis/character.ts
+++ fourtoon-cookie/src/apis/character.ts
@@ -1,16 +1,19 @@
 import { API_METHOD_TYPE, API_STATUS } from "../constants/api";
 import { ApiError } from "../error/ApiError";
 import type { Character } from "../types/character";
-import type { CharacterSavedResponse } from "../types/dto/character";
 import { requestApi } from "./api";
 
 export const getCharacters = async (): Promise<Character[]> => {
     const response = await requestApi(`/character`, API_METHOD_TYPE.GET);
-
     if (response.status !== API_STATUS.SUCCESS) {
-        throw new ApiError('캐릭터를 불러오는 중 오류가 발생했습니다. 잠시후 다시 시도해 주세요.', response.status);
+        throw new ApiError('캐릭터를 불러오는 중 오류가 발생했습니다. 잠시 후 다시 시도해 주세요.', response.status);
     }
 
-    const characterResponses: CharacterSavedResponse[] = await response.json();
-    return [...characterResponses];
+    const responseData = await response.json();
+
+    if (!Array.isArray(responseData.characterSavedResponses)) {
+        throw new ApiError('잘못된 응답 형식입니다. 캐릭터 목록을 불러오지 못했습니다.');
+    }
+
+    return responseData.characterSavedResponses as Character[];
 };
