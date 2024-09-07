6013b91ebdc42da9c94b32277f7830cf70a8b529
SproutMj <mjk54565@gmail.com>
2024-09-07 10:17:02 +0900

    refactor: status 제거

--- fourtoon-cookie/src/apis/character.ts
+++ fourtoon-cookie/src/apis/character.ts
@@ -7,7 +7,7 @@ import { requestApi } from "./api";
 export const getCharacters = async (): Promise<Character[]> => {
     const response = await requestApi(`/character`, API_METHOD_TYPE.GET);
     if (response.status !== API_STATUS.SUCCESS) {
-        throw new ApiError('캐릭터를 불러오는 중 오류가 발생했습니다. 잠시 후 다시 시도해 주세요.', response.status);
+        throw new ApiError('캐릭터를 불러오는 중 오류가 발생했습니다. 잠시 후 다시 시도해 주세요.');
     }
 
     const responseData = await response.json();
