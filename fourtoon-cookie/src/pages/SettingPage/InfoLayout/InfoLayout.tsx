895fffe2fa64b25c07cf085f7c21503b111e36d5
JuneBridge <junebridge03@g.skku.edu>
2024-09-11 11:55:42 +0900

    refactor: SettingPage에서 member 상태 사용하도록 수정

--- fourtoon-cookie/src/pages/SettingPage/InfoLayout/InfoLayout.tsx
+++ fourtoon-cookie/src/pages/SettingPage/InfoLayout/InfoLayout.tsx
@@ -2,16 +2,12 @@ import { Image, Text, TouchableOpacity, View } from 'react-native';
 import * as S from "./InfoLayout.styled";
 import { NavigationProp, useNavigation } from '@react-navigation/native';
 import { RootStackParamList } from '../../../constants/routing';
-import type { Member } from '../../../types/member';
 import { useSelectedCharacterStore } from '../../../store/selectedCharacter';
+import { useMemberStore } from '../../../store/member';
 
+const InfoLayout = () => {
 
-export interface InfoLayoutProps {
-    member: Member | null;
-}
-
-const InfoLayout = (props: InfoLayoutProps) => {
-    const { member, ...rest } = props;
+    const { member } = useMemberStore();
 
     const navigation = useNavigation<NavigationProp<RootStackParamList>>();
     const { selectedCharacter } = useSelectedCharacterStore();
@@ -24,7 +20,7 @@ const InfoLayout = (props: InfoLayoutProps) => {
         <View style={S.styles.profileContainer}>
             <Image source={{uri: selectedCharacter?.selectionThumbnailUrl}} style={S.styles.profileImage} />
             <Text style={S.styles.name}>{member?.name}</Text>
-            <Text style={S.styles.email}>{member?.email}</Text>
+            <Text style={S.styles.email}>{null}</Text>
             <TouchableOpacity style={S.styles.changeCharacterButton} onPress={handleCharacterChangeButtonPress}>
                 <Text style={S.styles.changeCharacterText}>캐릭터 변경</Text>
             </TouchableOpacity>
--- fourtoon-cookie/src/pages/SettingPage/SettingPage.tsx
+++ fourtoon-cookie/src/pages/SettingPage/SettingPage.tsx
@@ -2,7 +2,6 @@ import React, { ReactNode, useContext, useEffect, useState } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
 import * as S from './SettingPage.styled';
 import type { Member } from '../../types/member';
-import { getMember } from '../../apis/member';
 import MenuLayout from './MenuLayout/MenuLayout';
 import { GlobalErrorInfoType } from '../../types/error';
 import MainPageLayout from '../../components/layout/MainPageLayout/MainPageLayout';
@@ -11,30 +10,11 @@ import handleError from '../../error/errorhandler';
 import { FOOTER_STATE } from '../../components/layout/MainPageLayout/Footer/Footer';
 
 const SettingPage = () => {
-	const [member, setMember] = useState<Member | null>(null);
-
-  	useEffect(() => {
-		const fetchMember = async () => {
-			try {
-				const memberData = await getMember();
-				setMember(memberData);
-			} catch (error) {
-				if (error instanceof Error) {
-                    handleError(
-						error,
-						GlobalErrorInfoType.ALERT
-					);
-                }
-			}
-		};
-	
-    	fetchMember();
-  	}, []);
 
 	return (
     	<MainPageLayout footerState={FOOTER_STATE.SETTING} >
 			<View style={S.styles.container}>
-				<InfoLayout member={member} />
+				<InfoLayout />
 				<MenuLayout />
 			</View>
     	</MainPageLayout>
