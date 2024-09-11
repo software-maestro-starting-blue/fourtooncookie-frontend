e311837a4bea4cf839416ae6564584136a9e0e03
JuneBridge <junebridge03@g.skku.edu>
2024-09-11 15:57:44 +0900

    chore: 추후에 postDiary로 아이디를 받은 이후 로직을 처리해야 함 명시함

--- fourtoon-cookie/src/store/diaryList.ts
+++ fourtoon-cookie/src/store/diaryList.ts
@@ -53,7 +53,7 @@ export const useDiaryListStore = create<DiaryListState>(
 
         postDiary: async (diary: Diary) => {
             
-            await postDiary(diary.characterId, diary.diaryDate, diary.content);
+            await postDiary(diary.characterId, diary.diaryDate, diary.content); //TODO 생성된 일기 아이디를 받아야 함.
 
             const { diaryList } = get();
             await set({
