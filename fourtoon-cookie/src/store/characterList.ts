f93b3aa35cad5bab97e5d952b27610f0026176aa
JuneBridge <junebridge03@g.skku.edu>
2024-09-11 09:50:28 +0900

    fix: store의 정의 방식 변경 및 타입 추가

--- fourtoon-cookie/src/store/characterList.ts
+++ fourtoon-cookie/src/store/characterList.ts
@@ -3,28 +3,23 @@ import { getCharacters } from "../apis/character";
 import { jwtManager } from "../auth/jwt";
 import { Character } from "../types/character";
 
-const loadCharacterList = async () : Promise<Character[] | null> => {
-    try {
-        if (! jwtManager.getToken()) return null;
 
-        const responsedCharacterList = await getCharacters();
-        if (! responsedCharacterList){
-            throw Error('캐릭터를 불러오는 중 오류가 발생했습니다. 잠시후 다시 시도해 주세요.');
-        }
-
-        return responsedCharacterList;
-        
-    } catch (e) {
-        return null;
-    }
-};
-
-const updateCharacterList = async (set: setType) => {
-    set(await loadCharacterList());
+interface CharacterListState {
+    characterList: Character[];
+    updateCharacterList: () => Promise<void>;
 }
 
+export const useCharacterListStore = create<CharacterListState>(
+    (set) => ({
+        characterList: [],
 
-export const useCharacterListStore = create((set: setType) => ({
-    characterList: loadCharacterList(),
-    updateCharacterList: updateCharacterList(set)
+        updateCharacterList: async () => {
+            try {
+                const characterList = await getCharacters();
+
+                set({ characterList });
+            } catch (e) {
+                throw e;
+            }
+        }
 }));
--- fourtoon-cookie/src/store/selectedCharacter.ts
+++ fourtoon-cookie/src/store/selectedCharacter.ts
@@ -3,17 +3,19 @@ import { persist, createJSONStorage } from 'zustand/middleware'
 import { Character } from "../types/character";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-
-const setSelectedCharacter = (selectedCharacter: Character, set: setType) => {
-    set(selectedCharacter);
+interface SelectedCharacterState {
+    selectedCharacter: Character | null;
+    setSelectedCharacter: (selectedCharacter: Character) => void;
 }
 
-
 export const useSelectedCharacterStore = create( 
-    persist( 
-        (set: setType) => ({
+    persist<SelectedCharacterState>(
+        (set) => ({
             selectedCharacter: null,
-            setSelectedCharacter: (selectedCharacter: Character) => setSelectedCharacter(selectedCharacter, set),
+
+            setSelectedCharacter: (selectedCharacter: Character) => {
+                set({ selectedCharacter });
+            },
         }), 
         {
             name: 'selected-character-storage',
