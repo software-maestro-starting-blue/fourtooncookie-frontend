c42c7606ba81cdf2bed07087610d03efb9ff4f9f
JuneBridge <junebridge03@g.skku.edu>
2024-09-13 17:56:41 +0900

    chore: import 정리

--- fourtoon-cookie/src/pages/DiaryTimelinePage/DiaryComponent/DiaryComponent.tsx
+++ fourtoon-cookie/src/pages/DiaryTimelinePage/DiaryComponent/DiaryComponent.tsx
@@ -1,17 +1,11 @@
-import React, { useContext, useState } from "react";
+import React from "react";
 import { View } from "react-native";
-import { NavigationProp, useNavigation } from '@react-navigation/native';
-import ConfirmationModal from "../../../components/common/Modal/ConfirmationModal/ConfirmationModal";
-import { patchDiaryFavorite } from '../../../apis/diary';
 import { Diary } from "../../../types/diary";
 import DiaryContentsLayout from "./DiaryContentsLayout/DiaryContentsLayout";
 import DiaryPaintingImagesLayout from "./DiaryPaintingImagesLayout/DiaryPaintingImagesLayout";
 import * as S from './DiaryComponent.styled';
-import { RootStackParamList } from "../../../constants/routing";
-import { GlobalErrorInfoType } from "../../../types/error";
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
-import handleError from "../../../error/errorhandler";
 import DiaryPaintingImageLoadingLayout from "./DiaryPaintingImageLoadingLayout/DiaryPaintingImageLoadingLayout";
 
 export interface DiaryProps {
