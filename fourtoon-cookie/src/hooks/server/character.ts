ee0dc6de1630b36121a88261db470ae67ee8bcbd
JuneBridge <junebridge03@g.skku.edu>
2024-09-21 15:02:06 +0900

    feat: 특정 character를 Id를 활용해 찾는 훅 추가

--- fourtoon-cookie/src/hooks/server/character.ts
+++ fourtoon-cookie/src/hooks/server/character.ts
@@ -1,7 +1,16 @@
 import { useQuery } from "react-query"
 import { getCharacters } from "../../apis/character"
+import { Character } from "../../types/character";
 
 
 export const useCharacters = () => {
-    return useQuery("characters", getCharacters);
+    return useQuery<Character[], Error>("characters", getCharacters);
+}
+
+export const useCharacterById = (characterId: number) => {
+    return useQuery(["character", characterId], async () => {
+        const { data: characters } = useCharacters();
+
+        return characters?.find(character => character.id === characterId);
+    });
 }
