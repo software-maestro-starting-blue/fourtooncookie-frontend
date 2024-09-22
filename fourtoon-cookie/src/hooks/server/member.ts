6437982f43d409ce38425ba010004aadde8422e1
JuneBridge <junebridge03@g.skku.edu>
2024-09-23 00:25:47 +0900

    fix: 에러 발생 시 쿼리 무효화하는 로직 추가

--- fourtoon-cookie/src/hooks/server/member.ts
+++ fourtoon-cookie/src/hooks/server/member.ts
@@ -1,10 +1,22 @@
 import { useMutation, useQuery, useQueryClient } from "react-query";
 import { deleteMember, getMember, postMember } from "../../apis/member"
 import { Member } from "../../types/member";
+import { JwtError } from "../../error/JwtError";
 
 
 export const useMember = () => {
-    return useQuery<Member, Error>('member', () => getMember());
+    const queryClient = useQueryClient();
+
+    return useQuery<Member, Error>('member', getMember, {
+        refetchOnReconnect: true,
+        retry: false,
+        onError: (error) => {
+            if (error instanceof JwtError) {
+                queryClient.cancelQueries('member', { exact: true });
+                queryClient.removeQueries('member', { exact: true });
+            }
+        }
+    });
 }
 
 export const useCreateMember = () => {
