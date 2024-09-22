796797d4ef6b7d2dca67726e6f82bb4edd036fa1
JuneBridge <junebridge03@g.skku.edu>
2024-09-22 23:55:07 +0900

    refactor: requestApi에서 jwt를 별도로 받는 로직 삭제

--- fourtoon-cookie/src/apis/api.ts
+++ fourtoon-cookie/src/apis/api.ts
@@ -4,8 +4,8 @@ import { API_METHOD_TYPE, API_STATUS } from "../constants/api";
 import { useAccountStore } from "../store/account";
 
 
-export const requestApi = async (url: string, method: API_METHOD_TYPE, body?: any, jwtToken?: JWTToken): Promise<Response> => {
-    let token: JWTToken | null = jwtToken ? jwtToken : useAccountStore.getState().jwt;
+export const requestApi = async (url: string, method: API_METHOD_TYPE, body?: any): Promise<Response> => {
+    let token: JWTToken | null = useAccountStore.getState().jwt;
     
     let leftTryCount: number = 2;
 
