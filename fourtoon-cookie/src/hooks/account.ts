7cf0de34404cdc7decdb07fc6b4d2ab85e81a105
JuneBridge <junebridge03@g.skku.edu>
2024-09-23 00:03:13 +0900

    feat: AccountState를 관리하는 훅 제작

---
+++ fourtoon-cookie/src/hooks/account.ts
@@ -0,0 +1,71 @@
+import { useEffect, useState } from "react";
+import { AccountStatus } from "../types/account";
+import { JWTToken } from "../types/jwt";
+import { Member } from "../types/member";
+import { useCreateMember, useDeleteMember, useMember } from "./server/member";
+import { jwtManager } from "../auth/jwtManager";
+
+
+export const useAccountState = () => {
+    const [ accountState, setAccountState ] = useState<AccountStatus>(AccountStatus.UNAUTHORIZED);
+    const [ isLoading, setIsLoading ] = useState<boolean>(false);
+
+    const { data: member, refetch } = useMember();
+
+    const { mutate: createMember } = useCreateMember();
+    const { mutate: deleteMember } = useDeleteMember();
+
+    useEffect(() => {
+        if (isLoading) {
+            return;
+        }
+        
+        if (member) {
+            setAccountState(AccountStatus.LOGINED);
+            return;
+        }
+
+        if (jwtManager.getToken()) {
+            setAccountState(AccountStatus.UNSIGNEDUP);
+            return;
+        }
+
+        setAccountState(AccountStatus.UNAUTHORIZED);
+
+    }, [isLoading, member, jwtManager]);
+    
+    const login = async (token: JWTToken) => {
+        setIsLoading(true);
+        await jwtManager.setToken(token);
+        await refetch();
+        setIsLoading(false);
+    }
+
+    const signup = (member: Member) => {
+        setIsLoading(true);
+        createMember(member);
+        setIsLoading(false);
+    }
+
+    const logout = () => {
+        setIsLoading(true);
+        jwtManager.setToken(null);
+        setIsLoading(false);
+    }
+
+    const resign = () => {
+        setIsLoading(true);
+        deleteMember();
+        jwtManager.setToken(null);
+        setIsLoading(false);
+    }
+
+    return {
+        accountState,
+        login,
+        signup,
+        logout,
+        resign
+    }
+    
+}
