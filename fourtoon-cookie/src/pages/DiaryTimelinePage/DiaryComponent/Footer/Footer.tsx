94102edfb748d8696bfe5e273d557b1bdb18cdb9
minseo Jo <liging12@naver.com>
2024-09-23 22:24:11 +0900

    feat: Footer

--- fourtoon-cookie/src/pages/DiaryTimelinePage/DiaryComponent/Footer/Footer.tsx
+++ fourtoon-cookie/src/pages/DiaryTimelinePage/DiaryComponent/Footer/Footer.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Image, TouchableOpacity, View } from "react-native";
-
+// import Share from 'react-native-share';
+// import shareToKakao from "./share/shareToKakao";
 import FAVORITE_ACTIVATE_ICON from "../../../../../assets/icon/favorite-activate.png";
 import FAVORITE_INACTIVATE_ICON from "../../../../../assets/icon/favorite-inactivate.png";
 import DOWNLOAD_ICON from "../../../../../assets/icon/download.png";
 import UPLOAD_ICON from "../../../../../assets/icon/upload.png";
 import * as S from './Footer.styled';
-import { useUpdateDiaryFavorite } from "../../../../hooks/server/diary";
+import { useUpdateDiaryFavorite, useDownloadDiaryImage } from "../../../../hooks/server/diary";
+
 
 export interface FooterProps {
     diaryId: number;
@@ -17,10 +19,19 @@ const DiaryActionsLayout = (props: FooterProps) => {
     const {diaryId, isFavorite, ...rest} = props;
 
     const { mutate: updateDiaryFavorite } = useUpdateDiaryFavorite(diaryId);
+    const { data: downloadData, isLoading, refetch: downloadDiaryImage } = useDownloadDiaryImage(diaryId);
 
     const handleToggleFavorite = async () => {
         updateDiaryFavorite(! isFavorite);
     }
+
+    const handleDownload = async () => {
+        downloadDiaryImage();
+    };
+
+    const handleShare = async () => {
+
+    };
 
     return (
         <View style={S.styles.footer}>
@@ -30,6 +41,18 @@ const DiaryActionsLayout = (props: FooterProps) => {
                         source={isFavorite ? FAVORITE_ACTIVATE_ICON : FAVORITE_INACTIVATE_ICON}
                         style={S.styles.image}
                     />
+                </TouchableOpacity>
+            </View>
+
+            <View style={S.styles.actionButtons}>
+                <TouchableOpacity onPress={handleDownload}>
+                    <Image source={DOWNLOAD_ICON}
+                    style={S.styles.image} />
+                </TouchableOpacity>
+
+                <TouchableOpacity onPress={handleShare}>
+                    <Image source={UPLOAD_ICON}
+                    style={S.styles.image} />
                 </TouchableOpacity>
             </View>
             <View style={S.styles.actionButtons}>
