9bc126fe7d7a72dff5046d8dabb87bd6d4da5fb3
minseo Jo <liging12@naver.com>
2024-09-23 23:31:45 +0900

    feat: 공유하기

--- fourtoon-cookie/src/pages/DiaryTimelinePage/DiaryComponent/Footer/Footer.tsx
+++ fourtoon-cookie/src/pages/DiaryTimelinePage/DiaryComponent/Footer/Footer.tsx
@@ -1,14 +1,13 @@
 import React from "react";
-import { Image, TouchableOpacity, View } from "react-native";
-// import Share from 'react-native-share';
-// import shareToKakao from "./share/shareToKakao";
+import { Image, TouchableOpacity, View, Platform } from "react-native";
 import FAVORITE_ACTIVATE_ICON from "../../../../../assets/icon/favorite-activate.png";
 import FAVORITE_INACTIVATE_ICON from "../../../../../assets/icon/favorite-inactivate.png";
 import DOWNLOAD_ICON from "../../../../../assets/icon/download.png";
 import UPLOAD_ICON from "../../../../../assets/icon/upload.png";
 import * as S from './Footer.styled';
 import { useUpdateDiaryFavorite, useDownloadDiaryImage } from "../../../../hooks/server/diary";
-
+import Share from 'react-native-share';
+import * as FileSystem from 'expo-file-system';
 
 export interface FooterProps {
     diaryId: number;
@@ -20,6 +19,10 @@ const DiaryActionsLayout = (props: FooterProps) => {
 
     const { mutate: updateDiaryFavorite } = useUpdateDiaryFavorite(diaryId);
     const { data: downloadData, isLoading, refetch: downloadDiaryImage } = useDownloadDiaryImage(diaryId);
+    
+    // 목데이터 설정
+    const mockDownloadData = 'https://via.placeholder.com/250'
+    const finalDownloadData = downloadData || mockDownloadData; 
 
     const handleToggleFavorite = async () => {
         updateDiaryFavorite(! isFavorite);
@@ -29,8 +32,24 @@ const DiaryActionsLayout = (props: FooterProps) => {
         downloadDiaryImage();
     };
 
-    const handleShare = async () => {
 
+    const handleShare = async () => {
+        try {
+            const fileUri = finalDownloadData;
+            // 로컬에 있는 파일을 공유할 때 사용할 옵션 설정
+            const shareOptions = {
+                title: '다이어리 이미지 공유',
+                url:Platform.OS === 'ios' ? `file://${fileUri}` : fileUri, // ios, android
+                type: 'image/jpeg',
+                message: '다이어리 이미지를 공유합니다.',
+            };
+    
+            // 파일 공유 실행
+            const result = await Share.open(shareOptions);
+            console.log(result);
+        } catch (error) {
+            console.error('Error sharing image:', error);
+        }
     };
 
     return (
