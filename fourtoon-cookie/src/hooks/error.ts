d95e3818f39b4721ee2972b90b1e506b560ae669
JuneBridge <junebridge03@g.skku.edu>
2024-09-24 22:14:14 +0900

    feat: useAsyncFunctionWithErrorHandling 로직 추가

--- fourtoon-cookie/src/hooks/error.ts
+++ fourtoon-cookie/src/hooks/error.ts
@@ -26,3 +26,22 @@ export const useEffectWithErrorHandling = (effect: EffectCallback, deps?: Depend
         throw error;
     }
 }
+
+export const useAsyncFunctionWithErrorHandling = async (func: (...args: any[]) => Promise<any>) => {
+    const [error, setError] = useState<Error | null>(null);
+
+    if (error) {
+        throw error;
+    }
+
+    const execute = async (...args: any[]) => {
+        try {
+            return await func(...args);
+        } catch (error) {
+            if (! (error instanceof Error)) return;
+            setError(error);
+        }
+    }
+
+    return execute;
+}
