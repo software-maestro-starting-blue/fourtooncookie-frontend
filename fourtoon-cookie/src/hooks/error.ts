b1ff404c01093d21edebcea85d2fe9bed5a3f67d
JuneBridge <junebridge03@g.skku.edu>
2024-09-25 09:45:21 +0900

    refactor: useErrorThrower로 로직 통일

--- fourtoon-cookie/src/hooks/error.ts
+++ fourtoon-cookie/src/hooks/error.ts
@@ -1,8 +1,19 @@
+import { use } from "@js-joda/core";
 import { DependencyList, EffectCallback, useEffect, useState } from "react"
+
+const useErrorThrower = () => {
+    const [error, throwError] = useState<Error | null>(null);
+
+    if (error) {
+        throw error;
+    }
+
+    return [ throwError ];
+}
 
 
 export const useEffectWithErrorHandling = (effect: EffectCallback, deps?: DependencyList) => {
-    const [error, setError] = useState<Error | null>(null);
+    const [throwError] = useErrorThrower();
 
     useEffect(() => {
         const executeEffect = () => {
@@ -10,7 +21,7 @@ export const useEffectWithErrorHandling = (effect: EffectCallback, deps?: Depend
                 return effect();
             } catch (error) {
                 if (! (error instanceof Error)) return
-                setError(error);
+                throwError(error);
             }
         };
 
@@ -20,25 +31,17 @@ export const useEffectWithErrorHandling = (effect: EffectCallback, deps?: Depend
             if (cleanup instanceof Function) cleanup();
         };
     }, deps);
-
-    if (error) {
-        throw error;
-    }
 }
 
 export const useAsyncFunctionWithErrorHandling = async (func: (...args: any[]) => Promise<any>) => {
-    const [error, setError] = useState<Error | null>(null);
-
-    if (error) {
-        throw error;
-    }
+    const [throwError] = useErrorThrower();
 
     const execute = async (...args: any[]) => {
         try {
             return await func(...args);
         } catch (error) {
             if (! (error instanceof Error)) return;
-            setError(error);
+            throwError(error);
         }
     }
 
