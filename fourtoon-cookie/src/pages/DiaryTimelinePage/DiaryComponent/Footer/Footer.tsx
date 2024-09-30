7e49f1bd39025ccaee7855fc908aed50a1c12db5
minseo Jo <liging12@naver.com>
2024-09-30 14:51:17 +0900

    [feat] 일기 이미지 다운로드, 공유 기능 (#72)
    * feat: 파일시스템, 미디어라이브러리, 공유관련 빌드 파일
    
    * feat: Footer
    
    * feat: Footer.styled
    
    * feat: getDiaryImage - API
    
    * feat: useDownloadDiaryImage 리액트쿼리
    
    * feat: 공유 빌드 파일 (ios, android), 필요없는 빌드 제거
    
    * feat: 공유하기
    
    * feat: 이미지 파일 경로 jpg에서 png로 변경
    
    * feat: android, ios 갤러리 권한 추가
    
    * fix: 리액트 쿼리 버그 수정
    
    리액트 쿼리로 downloadDiaryImage를 걸면 타임라인 페이지에 있는 모든 일기를 바로 저장한다.
    
    물론 enabled를 이용해서 요리조리하면 막을 수 있겠지만 이것보다는 그냥 특정 일기의 이미지 다운 버튼을 누르면 다운 함수가 실행되도록 변경
    
    * feat: 이미지 공유 기능
    
    * fix: ios, android 파일 경로 구분
    
    이 부분은 해봐야 알음.
    
    * feat: 갤러리 권한이 없는 경우 권한 설정하는 기능
    
    * chore: 필요 없는 코드 제거
    
    * refactor: responesStatus 상수 이용
    
    * refactor: any 제거
    
    * refactor: os enum활용
    
    * feat: 공유 버튼을 닫은 경우 빠르게 리턴기능
    
    * refactor: system/images 로 분리
    
    * refactor: 메서드 분리 및 패키지 변경
    
    * chore: 공백 넣기
    
    * refactor: 리액트쿼리로 변경
    
    * refactor: 리액트 쿼리로 변경
    
    * chore: 주석제거
    
    * refactor: url 엔드포인트 변경
    
    /diary/${diaryId}/download을
    /diary/${diaryId}/image/full로 변경
    
    * chore: 필요없는 import 제거

--- fourtoon-cookie/src/pages/DiaryTimelinePage/DiaryComponent/Footer/Footer.tsx
+++ fourtoon-cookie/src/pages/DiaryTimelinePage/DiaryComponent/Footer/Footer.tsx
@@ -1,26 +1,67 @@
-import React from "react";
-import { Image, TouchableOpacity, View } from "react-native";
-
+import React, { useEffect, useState } from "react";
+import { Image, TouchableOpacity, View, Alert, Linking } from "react-native";
 import FAVORITE_ACTIVATE_ICON from "../../../../../assets/icon/favorite-activate.png";
 import FAVORITE_INACTIVATE_ICON from "../../../../../assets/icon/favorite-inactivate.png";
 import DOWNLOAD_ICON from "../../../../../assets/icon/download.png";
 import UPLOAD_ICON from "../../../../../assets/icon/upload.png";
 import * as S from './Footer.styled';
 import { useUpdateDiaryFavorite } from "../../../../hooks/server/diary";
+import { useDiaryFullImage } from "../../../../hooks/server/diary"; // React Query 훅 사용
+import { checkPhotoPermissions, saveBlobToFile, saveImageToGallery, shareImageFile } from "../../../../system/image";
 
 export interface FooterProps {
     diaryId: number;
     isFavorite: boolean;
 }
 
+enum ImageActionState {
+    NONE, DOWNLOAD, SHARE
+}
+
 const DiaryActionsLayout = (props: FooterProps) => {
-    const {diaryId, isFavorite, ...rest} = props;
+    const { diaryId, isFavorite, ...rest } = props;
 
     const { mutate: updateDiaryFavorite } = useUpdateDiaryFavorite(diaryId);
 
+    const [ imageActionState, setImageActionState ] = useState<ImageActionState>(ImageActionState.NONE);
+    const { data: diaryImageBlob } = useDiaryFullImage(diaryId, imageActionState != ImageActionState.NONE);
+
     const handleToggleFavorite = async () => {
-        updateDiaryFavorite(! isFavorite);
-    }
+        updateDiaryFavorite(!isFavorite);
+    };
+
+    useEffect(() => {
+        if (imageActionState == ImageActionState.NONE) return;
+        if (! diaryImageBlob) return;
+
+        const handleImageAction = async (imageActionState: ImageActionState) => {
+            try {
+                if (! await checkPhotoPermissions()) return;
+                const fileUri = await saveBlobToFile(diaryImageBlob, diaryId);
+
+                if (imageActionState == ImageActionState.DOWNLOAD) {
+                    await saveImageToGallery(fileUri);
+                    Alert.alert('이미지 저장 성공', '이미지를 갤러리에 저장했습니다.');
+                }
+
+                if (imageActionState == ImageActionState.SHARE) {
+                    await shareImageFile(fileUri);
+                }
+            } catch (error) {
+                if ( error instanceof Error && error.message == 'User did not share' ) return;
+
+                throw error;
+            }
+        };
+
+        handleImageAction(imageActionState);
+
+        setImageActionState(ImageActionState.NONE);
+    }, [imageActionState, diaryImageBlob]);
+
+    const handleDownload = () => setImageActionState(ImageActionState.DOWNLOAD);
+
+    const handleShare = () => setImageActionState(ImageActionState.SHARE);
 
     return (
         <View style={S.styles.footer}>
@@ -32,7 +73,15 @@ const DiaryActionsLayout = (props: FooterProps) => {
                     />
                 </TouchableOpacity>
             </View>
+
             <View style={S.styles.actionButtons}>
+                <TouchableOpacity onPress={handleDownload}>
+                    <Image source={DOWNLOAD_ICON} style={S.styles.image} />
+                </TouchableOpacity>
+
+                <TouchableOpacity onPress={handleShare}>
+                    <Image source={UPLOAD_ICON} style={S.styles.image} />
+                </TouchableOpacity>
             </View>
         </View>
     );
