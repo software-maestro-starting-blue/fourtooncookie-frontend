690454219cd6b4431322154f9ab2e46e00da5e34
JuneBridge <junebridge03@g.skku.edu>
2024-10-04 23:51:58 +0900

    feat: ErrorComponent 스타일 적용

---
+++ fourtoon-cookie/src/components/error/ErrorComponent/ErrorComponent.styled.ts
@@ -0,0 +1,8 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    safeArea: {
+      flex: 1,
+      backgroundColor: '#FFFFFF',
+    },
+});
--- fourtoon-cookie/src/components/error/ErrorComponent/ErrorComponent.tsx
+++ fourtoon-cookie/src/components/error/ErrorComponent/ErrorComponent.tsx
@@ -1,18 +1,22 @@
 import { FallbackProps } from "react-error-boundary";
 import { Text, TouchableOpacity, View } from "react-native";
 
+import * as S from "./ErrorComponent.styled";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const ErrorComponent = (props: FallbackProps) => {
     const { error, resetErrorBoundary } = props;
 
     return (
-        <TouchableOpacity onPress={() => resetErrorBoundary()}>
-            <View>
-                <Text>
-                    에러가 발생했습니다.
-                </Text>
-            </View>
-        </TouchableOpacity>
+        <SafeAreaView style={S.styles.area}>
+            <TouchableOpacity onPress={() => resetErrorBoundary()}>
+                <View>
+                    <Text>
+                        에러가 발생했습니다. 화면을 클릭하여 다시 시도해주세요.
+                    </Text>
+                </View>
+            </TouchableOpacity>
+        </SafeAreaView>
     );
 }
 
