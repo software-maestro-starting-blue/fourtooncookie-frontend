fef7c95f869086b62af448433ecad19c58186a3d
JuneBridge <junebridge03@g.skku.edu>
2024-10-15 19:01:41 +0900

    feat: BasicErrorBoundary에서 SelectedCharacterNotExistError일 경우에는 navigate하도록 로직 구성

--- fourtoon-cookie/src/components/error/BasicErrorBoundary/BasicErrorBoundary.tsx
+++ fourtoon-cookie/src/components/error/BasicErrorBoundary/BasicErrorBoundary.tsx
@@ -6,6 +6,9 @@ import { JwtError } from "../../../types/error/JwtError"
 import { ApiError } from "../../../types/error/ApiError"
 import { Alert } from "react-native"
 import { RuntimeError } from "../../../types/error/RuntimeError"
+import { SelectedCharacterNotExistError } from "../../../types/error/character/SelectedCharacterNotExistError"
+import { NavigationProp, useNavigation } from "@react-navigation/native"
+import { RootStackParamList } from "../../../types/routing"
 
 export interface BasicErrorBoundaryProps {
     handleErrorBeforeHandling?: (error: Error) => boolean,
@@ -17,6 +20,8 @@ const BasicErrorBoundary = (props: BasicErrorBoundaryProps) => {
     const { handleErrorBeforeHandling, handleErrorAfterHandling, children } = props;
 
     const [ effectHandler, setEffectHandler ] = useState<undefined | (() => void)>(undefined);
+
+    const navigation = useNavigation<NavigationProp<RootStackParamList>>();
 
     const { logout } = useAccountState();
 
@@ -53,6 +58,14 @@ const BasicErrorBoundary = (props: BasicErrorBoundaryProps) => {
                 Alert.alert("서버에서 문제가 발생하였습니다. 문제가 지속되면 관리자에게 알려주세요.", error.message);
                 return true;
             }
+        }
+
+        if (error instanceof SelectedCharacterNotExistError) {
+            setEffectHandler(() => {
+                Alert.alert("선택된 캐릭터가 존재하지 않습니다. 캐릭터 선택 화면으로 이동합니다.");
+                navigation.navigate("CharacterSelectPage");
+            });
+            return true;
         }
 
         if (error instanceof RuntimeError) {
