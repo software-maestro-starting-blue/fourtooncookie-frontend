1592de90872498f63d37d6bcf127d2dc581275b9
JuneBridge <junebridge03@g.skku.edu>
2024-10-15 15:04:29 +0900

    fix: 패키지 의존 변경 (react-native-localize -> expo-localization)
    최대한 네이티브 로직을 건드리지 않도록 로직을 수정함.

--- fourtoon-cookie/src/system/i18n.ts
+++ fourtoon-cookie/src/system/i18n.ts
@@ -1,25 +1,12 @@
-import i18n, { LanguageDetectorModule } from 'i18next';
+import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import * as RNLocalize from 'react-native-localize';
+import * as Localization from 'expo-localization';
 import en from '../../locales/en.json';
 import ko from '../../locales/ko.json';
 
-const languageDetector: LanguageDetectorModule = {
-  type: 'languageDetector',
-  detect: () => {
-    const locales = RNLocalize.getLocales();
-    if (locales && locales.length > 0) {
-      return locales[0].languageTag;
-    } else {
-      return 'en';
-    }
-  },
-  init: () => {},
-  cacheUserLanguage: () => {},
-};
+const deviceLanguage = Localization.getLocales()[0]?.languageCode || 'en';
 
 i18n
-  .use(languageDetector)
   .use(initReactI18next)
   .init({
     fallbackLng: 'en',
@@ -28,6 +15,7 @@ i18n
       en: { translation: en },
       ko: { translation: ko },
     },
+    lng: deviceLanguage,
     interpolation: {
       escapeValue: false,
     },
