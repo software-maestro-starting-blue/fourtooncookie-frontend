80922e86b3cee6b6c3a14e55d905141db757d3c7
JuneBridge <junebridge03@g.skku.edu>
2024-10-16 14:21:48 +0900

    refactor: IntroPage에 대한 컴포넌트 리팩토링 진행

---
+++ fourtoon-cookie/src/pages/IntroPage/AppleSignInAndSignUpButton.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Image, Platform, StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { appleAuthAndroid } from '@invertase/react-native-apple-authentication';
+import { getAppleIdToken } from "../../auth/appleOAuth";
+import { OAuthProvider } from "../../types/oauth";
+import { OS } from "../../types/os";
+
+import { useFunctionWithErrorHandling } from '../../hooks/error';
+import { useTranslationWithParentName } from '../../hooks/locale';
+
+export interface AppleSignInAndSignUpButtonProps {
+    onSuccess: (oauthProvider: OAuthProvider, idToken: string, nonce?: string) => void;
+}
+
+const AppleSignInAndSignUpButton = (props: AppleSignInAndSignUpButtonProps) => {
+    const {onSuccess, ...rest} = props;
+
+    const { asyncFunctionWithErrorHandling } = useFunctionWithErrorHandling();
+
+    const t = useTranslationWithParentName('pages.introPage.login');
+
+    const handlePress = asyncFunctionWithErrorHandling(async () => {
+        const [idToken, nonce] = await getAppleIdToken();
+        if (!idToken) return;
+        onSuccess(OAuthProvider.APPLE, idToken, nonce);
+    });
+
+    return (Platform.OS === OS.IOS || appleAuthAndroid.isSupported) && (
+        <TouchableOpacity style={styles.appleButton} onPress={handlePress}>
+            <Image source={require('../../../../assets/icon/apple.png')} style={styles.appleLogo} />
+            <Text style={styles.appleButtonText}>{t("apple")}</Text>
+        </TouchableOpacity>
+            );
+}
+
+export default AppleSignInAndSignUpButton;
+
+const styles = StyleSheet.create({
+    appleButton: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 19,
+        backgroundColor: '#000000',
+        borderRadius: 16,
+      },
+      appleLogo: {
+        width: 24,
+        height: 24,
+        position: 'absolute',
+        left: 18,
+      },
+      appleButtonText: {
+        fontFamily: 'Pretendard',
+        fontStyle: 'normal',
+        fontWeight: '600',
+        fontSize: 17,
+        color: '#FFFFFF',
+      },
+})
--- fourtoon-cookie/src/pages/IntroPage/IntroPage.tsx
+++ fourtoon-cookie/src/pages/IntroPage/IntroPage.tsx
@@ -1,13 +1,12 @@
-import { View, Image, Text } from "react-native";
+import { View, Image, Text, StyleSheet } from "react-native";
 import { NavigationProp, useNavigation } from "@react-navigation/native";
 import { supabaseSignInAndSignUpWithIdToken } from "../../auth/supabase";
 import { RootStackParamList } from "../../types/routing";
 import { OAuthProvider } from "../../types/oauth";
 import type { JWTToken } from "../../types/jwt";
 
-import AppleSignInAndSignUpButton from "./AppleSignInAndSignUpButton/AppleSignInAndSignUpButton";
-import GoogleSignInAndSignUpButton from "./GoogleSignInAndSignUpButton/GoogleSignInAndSignUpButton";
-import * as S from './IntroPage.styled';
+import AppleSignInAndSignUpButton from "./AppleSignInAndSignUpButton";
+import GoogleSignInAndSignUpButton from "./GoogleSignInAndSignUpButton";
 import { AccountStatus } from "../../types/account";
 import { useAccountState } from "../../hooks/account";
 import { useEffectWithErrorHandling, useFunctionWithErrorHandling } from "../../hooks/error";
@@ -45,14 +44,14 @@ const IntroPage = () => {
     });
 
     return (
-        <View style={S.styles.container}>
-            <View style={S.styles.header}>
-                <View style={S.styles.logoContainer}>
-                    <Image source={require('../../../assets/logo/logo-5.png')} style={S.styles.logo} />
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <View style={styles.logoContainer}>
+                    <Image source={require('../../../assets/logo/logo-5.png')} style={styles.logo} />
                 </View>
-                <Text style={S.styles.subtitle}>{t("subtitle")}</Text>
+                <Text style={styles.subtitle}>{t("subtitle")}</Text>
             </View>
-            <View style={S.styles.buttonsContainer}>
+            <View style={styles.buttonsContainer}>
                 <GoogleSignInAndSignUpButton onSuccess={handleSignUpAndSignInSuccess} />
                 <AppleSignInAndSignUpButton onSuccess={handleSignUpAndSignInSuccess} />
             </View>
@@ -61,3 +60,40 @@ const IntroPage = () => {
 }
 
 export default IntroPage;
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#FFFFFF',
+    },
+    header: {
+      flex: 0.9,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    logoContainer: {
+      width: 200,
+      height: 150,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    logo: {
+      width: '100%',
+      height: '100%',
+      resizeMode: 'contain',
+    },
+    subtitle: {
+      fontFamily: 'Pretendard',
+      fontWeight: '500',
+      fontSize: 14,
+      lineHeight: 20,
+      color: '#AAAAAA',
+      marginTop: 8,
+    },
+    buttonsContainer: {
+      position: 'absolute',
+      bottom: 60,
+      left: 20,
+      right: 20,
+    },
+    });
