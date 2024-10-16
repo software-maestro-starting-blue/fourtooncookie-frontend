dfc583a99e772d53722d60c9b8477524672e53d6
JuneBridge <junebridge03@g.skku.edu>
2024-10-16 23:51:08 +0900

    feat: ErrorBoundary에서 Alert 대신 Toast를 통해 정보를 전달하도록 로직 수정

--- fourtoon-cookie/src/components/error/BasicErrorBoundary.tsx
+++ fourtoon-cookie/src/components/error/BasicErrorBoundary.tsx
@@ -4,12 +4,12 @@ import { ReactNode, useEffect, useState } from "react"
 import { useAccountState } from "../../hooks/account"
 import { JwtError } from "../../types/error/JwtError"
 import { ApiError } from "../../types/error/ApiError"
-import { Alert } from "react-native"
 import { RuntimeError } from "../../types/error/RuntimeError"
 import { useTranslationWithParentName } from "../../hooks/locale"
 import { SelectedCharacterNotExistError } from "../../types/error/character/SelectedCharacterNotExistError"
 import { NavigationProp, useNavigation } from "@react-navigation/native"
 import { RootStackParamList } from "../../types/routing"
+import { showErrorToast, showInfoToast } from "../../system/toast"
 
 export interface BasicErrorBoundaryProps {
     handleErrorBeforeHandling?: (error: Error) => boolean,
@@ -47,31 +47,31 @@ const BasicErrorBoundary = (props: BasicErrorBoundaryProps) => {
         if (error instanceof ApiError) {
             const status = error.getStatus();
             if (status === null) {
-                Alert.alert(t("default"), error.message);
+                showErrorToast(t("default"), error.message);
                 return true;
             }
 
             if (400 <= status && status <= 499) {
-                Alert.alert(t("user"), error.message);
+                showErrorToast(t("user"), error.message);
                 return true;
             }
 
             if (500 <= status && status <= 599) {
-                Alert.alert(t("server"), error.message);
+                showErrorToast(t("server"), error.message);
                 return true;
             }
         }
 
         if (error instanceof SelectedCharacterNotExistError) {
             setEffectHandler(() => {
-                Alert.alert(t("characterNotSelected"));
+                showInfoToast(t("characterNotSelected"));
                 navigation.navigate("CharacterSelectPage");
             });
             return true;
         }
 
         if (error instanceof RuntimeError) {
-            Alert.alert(error.message);
+            showErrorToast(error.message);
             return true;
         }
 
